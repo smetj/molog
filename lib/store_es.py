818654d5b6c03f03b794b6ba6562a8736941bb2a
Jelle Smet <development@smetj.net>
2012-05-20 23:54:11 +0200

    Added JSON verification and ElasticSearch support.

---
+++ lib/store_es.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#       store_es.py
+#       
+#       Copyright 2012 Jelle Smet development@smetj.net
+#       
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 3 of the License, or
+#       (at your option) any later version.
+#       
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#       
+#       You should have received a copy of the GNU General Public License
+#       along with this program; if not, write to the Free Software
+#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#       MA 02110-1301, USA.
+#       
+#       
+
+from wishbone.toolkit import PrimitiveActor
+import pyes
+from gevent import monkey; monkey.patch_all()
+
+class StoreES(PrimitiveActor):
+    '''SToreES is a Wisbone module which stores incoming data into ElasticSearch.
+    
+    Parameters:        
+
+        * host: The ES host to connect to.
+        * user: The username to authenticate.
+        * password: The password to authenticate.
+    
+    '''
+    
+    def __init__(self, name, *args, **kwargs):
+        PrimitiveActor.__init__(self, name)
+        self.host = kwargs.get('host','localhost')
+        
+    def consume(self,doc):
+        self.conn.index(data,"logstash-%s.%s.%s"%(date.today().year, date.today().month, date.today().day),data['@type'])
+        
